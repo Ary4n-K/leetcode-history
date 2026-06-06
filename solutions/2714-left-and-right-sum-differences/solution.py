badda44914b1af4ebaeb907394a281ce2ecb0ffa
Aryan <aryankumar10505@gmail.com>
2026-06-06 07:14:41 +0000

    Sync LeetCode submission - Left and Right Sum Differences - Runtime - 1 ms (76.18%), Memory - 19.3 MB (96.54%)

---
+++ solutions/2714-left-and-right-sum-differences/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        left_sum = 0
+        right_sum = sum(nums) 
+        res = []
+        
+        for num in nums:
+            
+            right_sum -= num  
+            
+            res.append(abs(left_sum - right_sum))
+            
+           
+            left_sum += num   
+            
+        return res
