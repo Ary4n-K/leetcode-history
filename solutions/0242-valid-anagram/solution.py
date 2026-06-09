eb639c7077ffafa2ab7ac07464eb005ba754cf34
Aryan <aryankumar10505@gmail.com>
2026-06-09 17:48:26 +0000

    Sync LeetCode submission - Valid Anagram - Runtime - 7 ms (91.04%), Memory - 19.6 MB (27.76%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        if Counter(s)==Counter(t):
-            return True
-        return False
+        if len(s) != len(t):
+            return False
+        d1 = Counter(s)
+        d2 = Counter(t)
+        return d1 == d2
